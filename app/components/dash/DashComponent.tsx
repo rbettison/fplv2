0c38ae6591866a34fc537f90f19419ce8477fabd
rbettison <rob.bettison94@gmail.com>
2024-02-19 18:15:47 +0100

    adding pitch image and message when user has no leagues

--- app/components/dash/DashComponent.tsx
+++ app/components/dash/DashComponent.tsx
@@ -6,12 +6,12 @@ import AsItStands from "./AsItStands";
 import SeasonTracker from "./SeasonTracker";
 
 export default function DashComponent() {
-    const {selectedLeague, gameWeek, pointsHistory} = useContext(LeagueContext) as LeagueContextType;
+    const {selectedLeague, gameWeek, pointsHistory, leagues} = useContext(LeagueContext) as LeagueContextType;
 
 
     return (
         <div className="min-h-full w-full bg-slate-200 rounded-lg p-5 shadow-md flex flex-col justify-center items-center">
-            <div className="font-bold text-4xl mb-4">{selectedLeague.leagueName}</div>
+            {leagues.length > 0 ? <><div className="font-bold text-4xl mb-4">{selectedLeague.leagueName}</div>
             <div className="font-semibold text-2xl border-b-2 border-current mb-2">Gameweek {gameWeek} report</div>
             <Awards />
             <AsItStands />
@@ -19,8 +19,8 @@ export default function DashComponent() {
                 pointsHistory != null && <SeasonTracker />
 
 
-            }
-            
+            }</> : <div>Add a league to your dashboard to get started.</div>
+        }
         </div>
     )
 }
